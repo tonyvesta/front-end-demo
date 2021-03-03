913a47c66f66fb855b22d587d8a5625f11b26411
Tony Jones <Tony_Jones@waters.com>
2021-03-03 10:41:25 +0000

    stryker should now fail on error.

--- stryker.conf.js
+++ stryker.conf.js
@@ -11,6 +11,7 @@ module.exports = {
     },
     reporters: ['progress', 'clear-text', 'html'],
     coverageAnalysis: 'off',
+    "thresholds": { "high": 80, "low": 60, "break": 99.9 }
     // mutator: {
     //   plugins: ['@babel/plugin-syntax-jsx'],
     // },
